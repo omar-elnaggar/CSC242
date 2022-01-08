f9cde5a51bc5de44315428434d4802f1683d0404
omar.elnaggar <elnaggaros@gmail.com>
2022-01-07 19:34:22 -0800

    fixed a rounding error

--- P1-Restaurant Tip.cpp
+++ P1-Restaurant Tip.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+double roundToNextInteger(double x){
+	int integer = x;
+	double thousands = (x - integer) * 1000.0;
+	int hundreds = thousands / 10;
+	int thousandsPlace = ((int)thousands - hundreds*10);
+
+	if (thousandsPlace > 0){
+		return integer + (hundreds + 1.0)/100.0;
+	} else {
+		return integer + hundreds/100.0;
+	}
+}
+
 // main() program execution begins.
 int main() {
 	double billAmount = 0.0;
@@ -16,12 +29,12 @@ int main() {
 
 	double total = tipAmount + billAmount; // Add tip + bill to get the total
 
-	cout << "You need to pay " << 
-		total << 
-		" that is " << 
-		billAmount << 
-		" for the food + " << 
-		tipAmount << 
+	cout << "You need to pay $" << 
+		roundToNextInteger(total) << 
+		" that is $" << 
+		roundToNextInteger(billAmount) << 
+		" for the food + $" << 
+		roundToNextInteger(tipAmount) << 
 		" as tip." << 
 		endl; // print the output
 
